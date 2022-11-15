088f21b2499c2d72130e3c884c6e67ea2c8b5982
N3mEee <n3mebusiness@gmail.com>
2022-11-15 21:09:17 +0100

    Add delete button and LocalStorage

--- index.js
+++ index.js
@@ -12,17 +12,34 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function getBooksFromLocalStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+        myLibrary.push(JSON.parse(localStorage.getItem(`book${i}`)));
+    }
+}
+getBooksFromLocalStorage();
+
+function setBooksToLocalStorage() {
+    localStorage.clear();
+    for (let i = 0; i < myLibrary.length; i++) {
+        localStorage.setItem(`book${i}`, JSON.stringify(myLibrary[i]));
+    }
+}
+setBooksToLocalStorage();
+
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
+    setBooksToLocalStorage();
 }
 
 function displayBook() {
     const $table = document.querySelector("tbody");
     $table.innerHTML = `<tr>
                             <th>Book</th>
-                            <th>Pages</th>
                             <th>Author</th>
+                            <th>Pages</th>
                             <th>Read?</th>
+                            <th>Edit</th>
                         </tr>`;
 
     for (let i = 0; i < myLibrary.length; i++) {
@@ -31,20 +48,25 @@ function displayBook() {
         const $author = document.createElement("td");
         const $pages = document.createElement("td");
         const $read = document.createElement("td");
+        const $delete = document.createElement("td");
 
         $title.textContent = myLibrary[i].title;
         $author.textContent = myLibrary[i].author;
         $pages.textContent = myLibrary[i].pages;
         $read.textContent = myLibrary[i].read;
+        $delete.innerHTML = `<button class="delete">Delete</button>`;
+        $delete.children[0].setAttribute("data-id", `${i}`);
 
         $row.appendChild($title);
         $row.appendChild($author);
         $row.appendChild($pages);
         $row.appendChild($read);
+        $row.appendChild($delete);
 
         $table.appendChild($row);
     }
 }
+displayBook();
 
 $createButton.addEventListener("click", function () {
     if (!$form.classList.contains("form-container-show")) {
@@ -61,11 +83,16 @@ $addButton.addEventListener("click", function (event) {
     const $inputPages = document.querySelector("input[name=pages]");
     const $inputRead = document.querySelector("input[name=read]");
     if ($inputTitle.validity.valueMissing || $inputAuthor.validity.valueMissing || $inputPages.validity.valueMissing) {
-        const $pError = document.querySelector(".p-error")
+        const $pError = document.querySelector(".p-error");
         $pError.textContent = "Invalid title, author or pages";
     } else {
         addBookToLibrary($inputTitle.value, $inputAuthor.value, $inputPages.value, $inputRead.checked);
         displayBook();
+
+        $inputTitle.value = "";
+        $inputAuthor.value = "";
+        $inputPages.value = "";
+        $inputRead.checked = false;
 
         if (!$form.classList.contains("form-container-show")) {
             $form.classList.add("form-container-show");
@@ -80,5 +107,13 @@ $cancelButton.addEventListener("click", function (event) {
         $form.classList.add("form-container-show");
     } else {
         $form.classList.remove("form-container-show");
+    }
+});
+
+document.querySelector("body").addEventListener("click", function (event) {
+    if (event.target.className === "delete") {
+        myLibrary.splice(event.target.dataset.id, 1);
+        setBooksToLocalStorage();
+        displayBook();
     }
 });
